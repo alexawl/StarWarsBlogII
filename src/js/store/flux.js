5d4502768d124cc82997ac13079be1ea46d1d8c0
alexawl <alexawl@bellsouth.net>
2019-12-05 03:55:48 +0000

    2.0 first commit

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,42 +1,15 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			favorites: []
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
+			addToFavorites: element => {
+				let store = getStore();
+				const temp = store.favorites.concat([element]);
+				setStore({
+					favorites: temp
 				});
-
-				//reset the global store
-				setStore({ demo: demo });
 			}
 		}
 	};
